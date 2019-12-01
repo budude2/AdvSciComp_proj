88ebd9cea58509880447345c92b8e207fe14157a
Jacob Boline <mail@jboline.us>
2019-12-01 10:43:23 -0600

    Pass array size to the program.

--- main.cpp
+++ main.cpp
@@ -8,48 +8,53 @@ using namespace arma;
 
 int main(int argc, char *argv[])
 {
-    mat A = zeros<dmat>(2, 3);
+    unsigned long rows = strtoul(argv[2], nullptr, 10);
+    unsigned long cols = strtoul(argv[3], nullptr, 10);
+    cout << "Rows: " << rows << endl;
+    cout << "Cols: " << cols <<endl;
+
+    mat A = zeros<dmat>(rows, cols);
+    ifstream file;
 
     char * memblock;
-    unsigned long size = 0;
-    ifstream file;
-    long unsigned int itr = 0;
-
-    double tmp;
     double * double_values;
+    unsigned long size = 0;
+    unsigned long int itr = 0;
+    double tmp;
 
+
+    // Setup the file for reading in binary mode.
     file.open(argv[1], ios::in | ios::binary | ios::ate);
 
     if (file.is_open())
     {
+        // Get the size of the file in bytes
         size = file.tellg();
 
         cout << "\nsize = " << size << "\n";
 
+        // Allocate the memory block, read the entire file, close the file.
         memblock = new char [size];
         file.seekg(0, ios::beg);
         file.read(memblock, size);
         file.close();
 
+        // Convert the memblock into a double
         double_values = (double*) memblock;
-
-        for(itr = 0; itr < 6; itr++)
-        {
-            cout << "itr: " << itr << ", " << double_values[itr] << "\n";
-        }
 
         file.close();
     }
 
-    for(int r = 0; r < 3; r++)
+    // Iterate through the array and convert it to a matrix.
+    for(int c = 0; c < cols; c++)
     {
-        for(int c = 0; c < 2; c++)
+        for(int r = 0; r < rows; r++)
         {
-            cout << double_values[r * 2 + c] << " ";
-            A(c, r) = double_values[r * 2 + c];
+            A(r, c) = double_values[c * rows + r];
         }
-        cout << "\n";
     }
+
+    delete[] memblock;
 
     cout << "\n\n\n";
     A.print("A:");
@@ -58,6 +63,5 @@ int main(int argc, char *argv[])
     cout << "A(2,2): " << A(1, 1) << endl;
     cout << "A(1,2): " << A(0, 1) << endl;
     
-    delete[] memblock;
 	return 0; 
 }

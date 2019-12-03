a97112712b1d9304354badb8b8e87adf94870e55
Jacob <mail@jboline.us>
2019-12-03 17:22:16 -0600

    Remove unneeded stuff

--- MAGMA/support.h
+++ MAGMA/support.h
@@ -4,8 +4,6 @@
     #include<armadillo>
 
     double readFile(const char *, double *);
-    int readA(const char * filename, arma::Mat<double>& A, unsigned long rows, unsigned long cols);
-    int readb(const char * filename, arma::Mat<double>& b, unsigned long rows, unsigned long cols);
 
 #endif
 
